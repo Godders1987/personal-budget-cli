71fc058bdf89ff009dedf2df110a603f8dff1ba1
Joe Godwin <joe.godwin87@gmail.com>
2026-02-27 06:38:22 +0000

    Initial commit with gitignore

---
+++ main.py
@@ -0,0 +1,49 @@
+from decimal import Decimal
+
+def main():
+  # Set monthly amount
+  month_amt = get_month_amt('amount.txt')
+  # Keeps loop open
+  while True:
+    command = input('What would you like to do? (Set, Add, Balance, Reset or Exit): ')
+    if command == 'exit':
+      file = open('amount.txt', 'w')
+      file.write(str(month_amt))
+      file.close()
+      break
+    elif command == 'set':
+      month_amt = set_month_amt()
+      print('Your monthly amount has been set at £{}.'.format(month_amt))
+    elif command == 'add':
+      month_amt = expense(month_amt)
+      print('Your remaining budget is £{}'.format(month_amt))
+    elif command == 'reset':
+      file = open('amount.txt', 'w')
+      file.write('0')
+      file.close()
+      break
+    elif command == 'balance':
+      print('Your remaining monthly balance is £{}.'.format(month_amt))
+
+# Sets the monthly spend limited decidied by user
+def set_month_amt():
+  amt = input('What would you like to set your monthly budget at?: ')
+  amt = Decimal(amt)
+  return amt
+
+# Minuses off spend from monthly budget and returns new amount
+def expense(value):
+  spend = input('How much have you spent?: ')
+  spend = Decimal(spend)
+  new_amt = value - spend
+  return new_amt
+
+# Reads the tex file that has the current remaining amount saved
+def get_month_amt(filepath):
+   with open(filepath) as x:
+      month_amt = x.read()
+      month_amt = Decimal(month_amt)
+      return month_amt
+
+
+main()
