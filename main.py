6ebbc2042906b87f29ef3b1c601d07e74e4ee2e5
Joe Godwin <joe.godwin87@gmail.com>
2026-02-28 17:45:59 +0000

    Forced input to lowercase and implemented amount of days until payday calc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from datetime import date
 
 def main():
   # Set monthly amount
@@ -6,6 +7,7 @@ def main():
   # Keeps loop open
   while True:
     command = input('What would you like to do? (Set, Add, Balance, Reset or Exit): ')
+    command = command.lower()
     if command == 'exit':
       file = open('amount.txt', 'w')
       file.write(str(month_amt))
@@ -21,9 +23,12 @@ def main():
       file = open('amount.txt', 'w')
       file.write('0')
       file.close()
+      print('Your monthly balance has been reset to £0.')
       break
     elif command == 'balance':
-      print('Your remaining monthly balance is £{}.'.format(month_amt))
+      days_left = payday()
+      print('Your remaining monthly balance is £{} and you have {} days until payday.'.format(month_amt, days_left))
+      
 
 # Sets the monthly spend limited decidied by user
 def set_month_amt():
@@ -44,6 +49,25 @@ def get_month_amt(filepath):
       month_amt = x.read()
       month_amt = Decimal(month_amt)
       return month_amt
+
+# Calculates when the next payday is and calculates how many days till then
+def payday():
+  # Get todays date
+  today = date.today()
+  # Payday
+  payday_day = 25
+  if today.day <= payday_day:
+    days_remaining = payday_day - today.day
+    return days_remaining
+  elif today.day > payday_day and today.month + 1 >= 12:
+    next_payday = date(today.year + 1, 1, 25)
+    return next_payday - today
+  else:
+    next_payday = date(today.year, today.month + 1, 25)
+    return(next_payday - today).days
+    
+
+
 
 
 main()
